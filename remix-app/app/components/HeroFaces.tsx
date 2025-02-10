bd0f5bbafa0e0675c4497194a7973a915612413f
Nicolas Belazaras <belazaras@live.com>
2025-02-10 22:02:58 +0100

    improved home for mobile.

--- remix-app/app/components/HeroFaces.tsx
+++ remix-app/app/components/HeroFaces.tsx
@@ -6,7 +6,7 @@ export function HeroFaces({}) {
       {/* <div className="absolute inset-0 flex items-center justify-center z-20 animate-spin"> */}
       {/*   <img src="https://cdn.sanity.io/images/8s9mg3nu/production/bfad833f57351d1273defa1c4d1531e53537d52c-465x465.png?w=2000&fit=max&auto=format&dpr=2" /> */}
       {/* </div> */}
-      <div className="z-20 absolute flex flex-col bottom-[14%] pl-6 sm:pl-8 md:pl-12 text-white uppercase">
+      <div className="z-20 absolute flex flex-col bottom-[10%] md:bottom-[14%] pl-6 sm:pl-8 md:pl-12 text-white uppercase">
         <img className="w-36 h-36 animate-slow-spin" src="https://cdn.sanity.io/images/8s9mg3nu/production/bfad833f57351d1273defa1c4d1531e53537d52c-465x465.png?w=2000&fit=max&auto=format&dpr=2" />
         <h1 className="font-semibold text-6xl max-w-[4em] drop-shadow-lg">
           Noelia Rufat
@@ -15,9 +15,9 @@ export function HeroFaces({}) {
           Representaciones
         </p>
       </div>
-      <div className="flex flex-col -mt-[30%] md:-mt-[4em] [&_img]:max-w-[340px] [&_img]:grayscale [&_img]:hover:grayscale-0 [&_img]:transition [&_img]:duration-250 [&_img]:brightness-50 [&_img]:hover:brightness-100">
+      <div className="flex flex-col -mt-[30%] md:-mt-[4em] [&_img]:max-w-[315px] [&_img]:md:max-w-[340px] [&_img]:grayscale [&_img]:hover:grayscale-0 [&_img]:transition [&_img]:duration-250 [&_img]:brightness-50 [&_img]:hover:brightness-100">
         <div className="w-full inline-flex flex-nowrap overflow-hidden">
-          <ul className="flex items-center justify-center md:justify-start [&_img]:max-w-[350px] animate-infinite-scroll">
+          <ul className="flex items-center justify-center md:justify-start animate-infinite-scroll">
             <li><img src="https://picsum.photos/500?random=1" alt="Random person"/></li>
             <li><img src="https://picsum.photos/500?random=2" alt="Random person" /></li>
             <li><img src="https://picsum.photos/500?random=3" alt="Random person" /></li>
